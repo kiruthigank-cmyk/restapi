feefadfad7a796b27273f197c93ece709325f9f1
kiruthigank-cmyk <kiruthigauthayakumar@gmail.com>
2026-02-02 14:12:23 +0530

    server

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose'
+import express from 'express'
+import cors from 'cors'
+import bodyParser from "body-parser"
+import dotenv from "dotenv"
+//import routes from './routes'
+
+const app =express();
+app.use(bodyParser.json());
+app.use(cors());
+dotenv.config();
+
+const PORT =process.env.PORT || 8009;
+const MONGOURL =process.env.MONGO_URL;
+mongoose.connect(MONGOURL).then(()=>{
+    console.log("mongodb connection success")
+    app.listen(PORT,()=>{
+        console.log(`server port listening at ${PORT}...`)
+    })
+}).catch((err)=>{
+    console.log("Error",err.toString())
+})
